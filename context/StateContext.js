1e10a44517a67049d562737fb338df22626becd7
youngjerald239 <youngjerald239@gmail.com>
2022-10-25 19:50:30 -0500

    created cart and add to cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,28 +1,37 @@
 import React, {createContext, useContext, useState, useEffect} from 'react'
-import { Toast } from 'react-hot-toast'
+import { toast } from 'react-hot-toast'
 
 const Context = createContext()
 
 export const StateContext = ({children}) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState()
+    const [cartItems, setCartItems] = useState([])
     const [totalPrice, setTotalPrice] = useState()
     const [totalQuantities, setTotalQuantities] = useState()
     const [qty, setQty] = useState(1)
     const onAdd = (product,quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id)
 
-        if (checkProductInCart) {
-            setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
-            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
 
+        if (checkProductInCart) {
+           
             const updatedCartItems = cartItems.map((cartProduct) => {
                 if(cartProduct._id === product._id) return {
                     ...cartProduct,
                     quantity: cartProduct.quantity + quantity
                 }
             })
+
+            setCartItems(updatedCartItems)
+            
+        }else{
+            product.quantity = quantity
+
+            setCartItems([...cartItems, {...product}])
         }
+        toast.success(`${qty} ${product.name} added to the cart!`)
     }
 
     const incQty = () => {
@@ -38,7 +47,7 @@ export const StateContext = ({children}) => {
     }
 
     return (
-        <Context.Provider value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty}}>
+        <Context.Provider value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd}}>
             {children}
         </Context.Provider>
     )
